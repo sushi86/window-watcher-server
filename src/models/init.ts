6d1820d5d176032bc10afd31e37c15a01f27b95d
Sascha Krauß <sascha.krauss@outlook.de>
2023-08-17 11:24:43 +0200

    Change to env service account

--- src/models/init.ts
+++ src/models/init.ts
@@ -2,18 +2,36 @@ import * as admin from 'firebase-admin';
 import * as serviceAccount from '../../config/service-account.json';
 import { getFirestore } from "firebase-admin/firestore";
 
-const params = {
-    type: serviceAccount.type,
-    projectId: serviceAccount.project_id,
-    privateKeyId: serviceAccount.private_key_id,
-    privateKey: serviceAccount.private_key,
-    clientEmail: serviceAccount.client_email,
-    clientId: serviceAccount.client_id,
-    authUri: serviceAccount.auth_uri,
-    tokenUri: serviceAccount.token_uri,
-    authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-    clientC509CertUrl: serviceAccount.client_x509_cert_url
+const environment = process.env.NODE_ENV || 'development';
+
+let params = {};
+if (environment == 'production') {
+  params = {
+    type: process.env.type,
+    projectId: process.env.project_id,
+    privateKeyId: process.env.private_key_id,
+    privateKey: process.env.private_key,
+    clientEmail: process.env.client_email,
+    clientId: process.env.client_id,
+    authUri: process.env.auth_uri,
+    tokenUri: process.env.token_uri,
+    authProviderX509CertUrl: process.env.auth_provider_x509_cert_url,
+    clientC509CertUrl: process.env.client_x509_cert_url
   }
+} else {
+  params = {
+      type: serviceAccount.type,
+      projectId: serviceAccount.project_id,
+      privateKeyId: serviceAccount.private_key_id,
+      privateKey: serviceAccount.private_key,
+      clientEmail: serviceAccount.client_email,
+      clientId: serviceAccount.client_id,
+      authUri: serviceAccount.auth_uri,
+      tokenUri: serviceAccount.token_uri,
+      authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
+      clientC509CertUrl: serviceAccount.client_x509_cert_url
+    }
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(params)
